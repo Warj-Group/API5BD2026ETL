17a3dc6cf36cab25368f2a6e3f6d71d1933b83d2
João Pedro <marcondesjpb@gmail.com>
2026-03-28 15:27:07 -0300

    feat/WARJ-31: Setting the order for when to upload files

--- load/__init__.py
+++ load/__init__.py
@@ -1,6 +1,10 @@
 import os
 import logging
+import pandas as pd
+from turtle import pd
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+load_dotenv()
 
 logger = logging.getLogger(__name__)
 
@@ -13,3 +17,20 @@ def get_engine():
 def run_load(transformed_data: dict) -> None:
     logger.info("Carregando dados no banco...")
     engine = get_engine()
+
+    for table_name, data in transformed_data.items():
+        if not data:
+            continue
+
+        df = pd.DataFrame(data)
+
+        logger.info(f"Inserindo dados na tabela {table_name}...")
+
+        df.to_sql(
+            name=table_name,
+            con=engine,
+            if_exists="append",
+            index=False
+        )
+
+    logger.info("Carga finalizada com sucesso!")
